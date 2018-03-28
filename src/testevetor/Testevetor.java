3435baee9eb386132783b49a3fa6bded389fb99c
IFSC <IFSC@CDR-770658.CEFETSC>
2018-03-28 20:27:10 -0300

    comecei meu codigo

--- src/testevetor/Testevetor.java
+++ src/testevetor/Testevetor.java
@@ -11,35 +11,8 @@ package testevetor;
  */
 public class Testevetor {
     /*construtor */
-    public Vetor (int size){}
-    
-    /*Prencher o vetor com valores aleatorios*/
-    public voi randomFill(){}
-    
-    /* Implementa o método de ordenação bubbleSort*/
-    public void bubbleSort(){}
-    
-    /* Convert o vetor para String*/
-    public String toString (){}
-    
-    /*Compara se dois vetores são iguais */
-    public boolean equals (int[] v){}
-    
-    /*Atribui um valor na posição pos do vetor */
-    public void setAT(int pos){}
-    
-    /*Retorna o valor na posição pos do vetor*/
-    public int valueAt (int pos ){}
-    
-    /*Retorna o tamanho do vetor*/
-    public int length() {}
-    
-    /* Concatena dois vetores */
-    public Vetor Concat (Vetor v){}
-    /**
-     * @param args the command line arguments */
-    /*public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args){
+        
     }
     
 }
---
+++ src/testevetor/Vetor.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testevetor;
+
+/**
+ *
+ * @author IFSC
+ */
+public class Vetor {
+      public Vetor (int size){
+        
+    }
+    
+    /*Prencher o vetor com valores aleatorios*/
+    public void randomFill(){
+        
+    }
+    
+    /* Implementa o método de ordenação bubbleSort*/
+    public void bubbleSort(){
+        
+    }
+    
+    /* Convert o vetor para String*/
+    public String toString (){
+        
+       return new String();   
+    }
+    
+    /*Compara se dois vetores são iguais */
+    public boolean equals (int[] v){
+        
+        return true;
+    }
+    
+    /*Atribui um valor na posição pos do vetor */
+    public void setAT(int pos){
+        
+    }
+
+    
+    /*Retorna o valor na posição pos do vetor*/
+   /* public int valueAt (int pos ){
+        return 0;
+    }*/
+    
+    /*Retorna o tamanho do vetor*/
+    public int length() {
+        return 0;
+    }
+    
+    /* Concatena dois vetores */
+    public Vetor Concat (Vetor v){
+        return null;
+    }
+   
+    /*public static void main(String[] args) {
+        // TODO code application logic here
+    }*/
+  
+}
