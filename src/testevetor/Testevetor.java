5674330299fc9e7ac9f9e1ce16ead76f976a6441
IFSC <IFSC@CDR-770658.CEFETSC>
2018-03-28 20:20:39 -0300

    comecei meu codigo

--- src/testevetor/Testevetor.java
+++ src/testevetor/Testevetor.java
@@ -10,11 +10,35 @@ package testevetor;
  * @author IFSC
  */
 public class Testevetor {
-
+    /*construtor */
+    public Vetor (int size){}
+    
+    /*Prencher o vetor com valores aleatorios*/
+    public voi randomFill(){}
+    
+    /* Implementa o método de ordenação bubbleSort*/
+    public void bubbleSort(){}
+    
+    /* Convert o vetor para String*/
+    public String toString (){}
+    
+    /*Compara se dois vetores são iguais */
+    public boolean equals (int[] v){}
+    
+    /*Atribui um valor na posição pos do vetor */
+    public void setAT(int pos){}
+    
+    /*Retorna o valor na posição pos do vetor*/
+    public int valueAt (int pos ){}
+    
+    /*Retorna o tamanho do vetor*/
+    public int length() {}
+    
+    /* Concatena dois vetores */
+    public Vetor Concat (Vetor v){}
     /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
+     * @param args the command line arguments */
+    /*public static void main(String[] args) {
         // TODO code application logic here
     }
     
